1665810174ae31d5cdb42314b4b07b2429d8d129
Carlo X Lopez <carlo.xavier.lopez@gmail.com>
2021-02-28 01:21:43 +0100

    load photo from url using Coil lib

--- app/src/main/java/com/puppy/adoption/lovegadse/app/ui/components/Detail.kt
+++ app/src/main/java/com/puppy/adoption/lovegadse/app/ui/components/Detail.kt
@@ -1,30 +1,34 @@
 package com.puppy.adoption.lovegadse.app.ui.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.puppy.adoption.lovegadse.app.data.DependenciesHolder
 import com.puppy.adoption.lovegadse.app.ui.theme.Typography
+import dev.chrisbanes.accompanist.coil.CoilImage
 
 @Composable
 fun Detail(dependencies: DependenciesHolder, puppyID: String, navController: NavController) {
     val puppy = dependencies.puppyRepository.findPuppy(puppyID)!!
     Column {
-        Image(
-            ColorPainter(Color.Blue),
+        CoilImage(
+            puppy.photoUrl,
             contentDescription = "$puppy picture",
+            contentScale = ContentScale.FillWidth,
             modifier = Modifier
                 .height(200.dp)
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            loading = {
+                Box(Modifier.matchParentSize()) {
+                    CircularProgressIndicator(Modifier.align(Alignment.Center))
+                }
+            }
         )
         Spacer(modifier = Modifier.height(8.dp))
 
--- app/src/main/java/com/puppy/adoption/lovegadse/app/ui/components/PuppyCard.kt
+++ app/src/main/java/com/puppy/adoption/lovegadse/app/ui/components/PuppyCard.kt
@@ -1,16 +1,16 @@
 package com.puppy.adoption.lovegadse.app.ui.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -18,6 +18,7 @@ import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
 import com.puppy.adoption.lovegadse.app.data.Puppy
 import com.puppy.adoption.lovegadse.app.ui.theme.Typography
+import dev.chrisbanes.accompanist.coil.CoilImage
 
 @Composable
 fun PuppyCard(
@@ -33,13 +34,19 @@ fun PuppyCard(
         elevation = 0.dp,
     ) {
         Column(modifier = Modifier.padding(8.dp)) {
-            Image(
-                ColorPainter(Color.Blue),
+            CoilImage(
+                puppy.photoUrl,
                 contentDescription = "$puppy picture",
+                contentScale = ContentScale.FillWidth,
                 modifier = Modifier
                     .height(200.dp)
                     .fillMaxWidth()
-                    .clip(shape = RoundedCornerShape(4.dp))
+                    .clip(shape = RoundedCornerShape(4.dp)),
+                loading = {
+                    Box(Modifier.matchParentSize()) {
+                        CircularProgressIndicator(Modifier.align(Alignment.Center))
+                    }
+                },
             )
             Spacer(modifier = Modifier.height(8.dp))
 
